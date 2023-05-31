43a396a8af2e826c8a0eaa05b75c236e1575ccd3
Mason Wrolstad <mason@surge.ai>
2023-05-31 13:50:10 -0700

    adding the initial files to main

---
+++ README.md
@@ -0,0 +1,127 @@
+# baseball_savant_scraper
+
+An easy tool to scrape the current odds listed on bettingdata.com
+
+### Use as a command line:
+
+```cmd
+python3 src/baseball_savant_scraper/__init__.py --sport "MLB" --game_date "05-21-2023"
+```
+
+### Import and use as a package:
+
+```python
+from baseball_savant_scraper import StatScraper
+import json
+stats_scraper = StatScraper(
+  statistics="expected_statistics",
+  type="pitcher",
+  year="2023",
+  min="25",
+)
+print(json.dumps(stats_scraper.stats, indent=2))
+[
+    {
+        "is_qualified": "0",
+        "year": "2023",
+        "group_type": "0",
+        "entity_name": "Then, Juan",
+        "entity_id": "672730",
+        "entity_team_name": "Mariners",
+        "entity_team_name_alt": "SEA",
+        "entity_team_id": "136",
+        "grouping_code": "Pitcher",
+        "pos": null,
+        "pos_group": null,
+        "pa": "31",
+        "bip": "25",
+        "launch_angle_avg": "-5.3",
+        "sweet_spot_percent": 20,
+        "exit_velocity_max": "106.1",
+        "exit_velocity_avg": "91",
+        "exit_velocity_fbld": "90.3",
+        "exit_velocity_gb": "91.5",
+        "distance_max": "365",
+        "distance_avg": "77",
+        "distance_hr_avg": null,
+        "hard_hit_ct": "11",
+        "hard_hit_percent": 44,
+        "hard_hit_per_swing": 25,
+        "barrel_ct": "0",
+        "barrels_per_bip": 0,
+        "barrels_per_pa": 0,
+        "barrels_per_swing": 0,
+        "ba": 0.241,
+        "est_ba": 0.295,
+        "slg": 0.241,
+        "est_slg": 0.347,
+        "woba": 0.245,
+        "est_woba": 0.308,
+        "wobacon": 0.248,
+        "est_wobacon": 0.327,
+        "xera": "3.87",
+        "era": "3.68",
+        "ba_minus_est_ba_diff": -0.05399999999999999,
+        "slg_minus_est_slg_diff": -0.10599999999999998,
+        "woba_minus_est_woba_diff": -0.063,
+        "wobacon_minus_est_wobacon_diff": -0.07900000000000001,
+        "era_minus_xera_diff": "-0.188",
+        "est_wobacon_minus_wobacon_diff": -0.07900000000000001,
+        "est_ba_minus_ba_diff": -0.05399999999999999,
+        "est_woba_minus_woba_diff": -0.063,
+        "est_slg_minus_slg_diff": -0.10599999999999998,
+        "href": "<a href=\"/savant-player/672730\">Then, Juan</a>"
+    },
+    {
+        "is_qualified": "0",
+        "year": "2023",
+        "group_type": "0",
+        "entity_name": "Soriano, George",
+        "entity_id": "666277",
+        "entity_team_name": "Marlins",
+        "entity_team_name_alt": "MIA",
+        "entity_team_id": "146",
+        "grouping_code": "Pitcher",
+        "pos": null,
+        "pos_group": null,
+        "pa": "30",
+        "bip": "25",
+        "launch_angle_avg": "11.5",
+        "sweet_spot_percent": 44,
+        "exit_velocity_max": "106.8",
+        "exit_velocity_avg": "89.6",
+        "exit_velocity_fbld": "94.3",
+        "exit_velocity_gb": "83.5",
+        "distance_max": "369",
+        "distance_avg": "166",
+        "distance_hr_avg": null,
+        "hard_hit_ct": "10",
+        "hard_hit_percent": 40,
+        "hard_hit_per_swing": 20.4,
+        "barrel_ct": "2",
+        "barrels_per_bip": 8,
+        "barrels_per_pa": 6.7,
+        "barrels_per_swing": 4.1,
+        "ba": 0.259,
+        "est_ba": 0.294,
+        "slg": 0.296,
+        "est_slg": 0.419,
+        "woba": 0.266,
+        "est_woba": 0.332,
+        "wobacon": 0.263,
+        "est_wobacon": 0.342,
+        "xera": "4.57",
+        "era": "2.57",
+        "ba_minus_est_ba_diff": -0.034999999999999976,
+        "slg_minus_est_slg_diff": -0.123,
+        "woba_minus_est_woba_diff": -0.066,
+        "wobacon_minus_est_wobacon_diff": -0.07900000000000001,
+        "era_minus_xera_diff": "-1.999",
+        "est_wobacon_minus_wobacon_diff": -0.07900000000000001,
+        "est_ba_minus_ba_diff": -0.034999999999999976,
+        "est_woba_minus_woba_diff": -0.066,
+        "est_slg_minus_slg_diff": -0.123,
+        "href": "<a href=\"/savant-player/666277\">Soriano, George</a>"
+    }
+]
+```
---
+++ src/baseball_savant_scraper/__init__.py
@@ -0,0 +1,127 @@
+import argparse
+import json
+import re
+import requests
+
+from bs4 import BeautifulSoup
+
+
+COOKIES = {
+    'AMCVS_A65F776A5245B01B0A490D44%40AdobeOrg': '1',
+    's_ecid': 'MCMID%7C79561376762042481851150433034797703144',
+    'AMCV_A65F776A5245B01B0A490D44%40AdobeOrg': '1099438348%7CMCIDTS%7C19502%7CMCMID%7C79561376762042481851150433034797703144%7CMCAAMLH-1685568541%7C9%7CMCAAMB-1685568541%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1684970941s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C2.1.0',
+    's_cc': 'true',
+    's_sq': '%5B%5BB%5D%5D',
+    '_dd_s': '',
+}
+
+HEADERS = {
+    'authority': 'baseballsavant.mlb.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9,ht;q=0.8',
+    'cache-control': 'no-cache',
+    'pragma': 'no-cache',
+    'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+}
+
+BASE_URL = "https://baseballsavant.mlb.com/leaderboard"
+REGEX = re.compile("(var data = )(.*)(;\\\n)")
+
+
+def scrape_stats(statistics: str, type: str, year: str, team: str = None, min: str = "q"):
+    response = requests.get(
+        f"{BASE_URL}/{statistics}",
+        params={
+            'type': type,
+            'year': year,
+            'position': '',
+            'team': team if team else '',
+            'min': min,
+        },
+        cookies=COOKIES,
+        headers=HEADERS,
+    )
+    print("Response:", response.status_code)
+
+    stats_soup = BeautifulSoup(response.text, features="lxml")
+    stats_scripts = stats_soup.select("script")
+    stats_dicts = [
+        stats.text for stats in stats_scripts 
+        if stats.text.strip().startswith("var data =")
+    ]
+
+    if len(stats_dicts) == 0:
+        print("Could not find the JSON object")
+        return []
+
+    stats_matches = REGEX.findall(stats_dicts[0])
+
+    return json.loads(stats_matches[0][1])
+
+
+class StatScraper:
+    def __init__(self, statistics: str, type: str, year: str, team: str = "", min: str = "q"):
+        try:
+            self.stats = scrape_stats(statistics, type, year, team, min)
+        except Exception as e:
+            print(f"An error occurred:\n{e}")
+            return
+
+
+def main(statistics: str, type: str, year: int, team: str = "", min: str = "q"):
+    games = StatScraper(statistics, type, year, team, min)
+    print(json.dumps(games.stats, indent=4))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=""
+    )
+    parser.add_argument(
+        "--statistics",
+        required=True,
+        help="The type of statistics we want to grab.",
+        choices=[
+            "expected_statistics"
+        ]
+    )
+    parser.add_argument(
+        "--type",
+        required=True,
+        help="The level of granularity of the statistics, either pitchers/batters or teams.",
+        choices=[
+            "pitcher",
+            "batter",
+            "pitcher-team",
+            "batter-team",
+        ]
+    )
+    parser.add_argument(
+        "--year",
+        required=True,
+        help="The year to grab the data for.",
+        default="2023",
+    )
+    parser.add_argument(
+        "--team",
+        required=False,
+        help="The specific team you'd like to get the stats for. A blank string is all teams.",
+        default=None,
+    )
+    parser.add_argument(
+        "--min",
+        required=False,
+        help="The minimum statistic to filter on.",
+        choices=["q", "1", "25", "50", "100", "150", "200"],
+        default="q",
+    )
+    args = parser.parse_args()
+    main(args.statistics, args.type, args.year, args.team, args.min)
